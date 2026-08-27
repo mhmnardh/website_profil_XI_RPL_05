e83c33cee25dc9370b3be0a2751642c61d51d237
evan <evanaditya1123@gmail.com>
2026-08-27 10:22:15 +0700

    Perubahan evan

--- resources/views/class/index.blade.php
+++ resources/views/class/index.blade.php
@@ -4,8 +4,8 @@
 
 @section('content')
 <div class="p-5 bg-light rounded-3 border">
-    <h1 class="display-6 fw-bold">Selamat Datang di Portal Kelas</h1>
-    <p class="fs-5 text-muted">Ini adalah website landing page untuk menampilkan profil kelas, daftar anggota kelompok, informasi proyek yang sedang dikerjakan, serta dokumentasinya.</p>
-    <a href="{{ route('class.kontak') }}" class="btn btn-primary">Lihat Informasi Proyek</a>
+    <h1 class="display-6 fw-bold">Selamat Datang di profil kelompok</h1>
+    <p class="fs-5 text-muted">Ini adalah website landing page untuk menampilkan profil kelompok, daftar anggota kelompok, dan kontak kelompok.</p>
+    <a href="{{ route('class.kontak') }}" class="btn btn-primary">Lihat Kontak</a>
 </div>
 @endsection
