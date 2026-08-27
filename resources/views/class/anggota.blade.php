20372f9a079e3c29818250ef6e0c14787064e1bb
Bunga <bungaayuni270@gmail.com>
2026-08-27 11:11:44 +0700

    perubahan bunga

--- resources/views/class/anggota.blade.php
+++ resources/views/class/anggota.blade.php
@@ -9,8 +9,8 @@
     <div class="col">
         <div class="card border shadow-sm">
             <div class="card-body text-center">
-                <h5 class="card-title fw-bold">Ahmad Rizky</h5>
-                <p class="card-text text-muted mb-1">NIM: 220101001</p>
+                <h5 class="card-title fw-bold">Muhaimin</h5>
+                <p class="card-text text-muted mb-1">Absen: 19</p>
                 <span class="badge bg-primary">Project Manager</span>
             </div>
         </div>
@@ -18,18 +18,27 @@
     <div class="col">
         <div class="card border shadow-sm">
             <div class="card-body text-center">
-                <h5 class="card-title fw-bold">Siti Aminah</h5>
-                <p class="card-text text-muted mb-1">NIM: 220101002</p>
-                <span class="badge bg-secondary">Frontend Developer</span>
+                <h5 class="card-title fw-bold">Bunga</h5>
+                <p class="card-text text-muted mb-1">Absen: 7</p>
+                <span class="badge bg-primary">Developer</span>
             </div>
         </div>
     </div>
     <div class="col">
         <div class="card border shadow-sm">
             <div class="card-body text-center">
-                <h5 class="card-title fw-bold">Budi Santoso</h5>
-                <p class="card-text text-muted mb-1">NIM: 220101003</p>
-                <span class="badge bg-dark">Backend Developer</span>
+                <h5 class="card-title fw-bold">Elsa</h5>
+                <p class="card-text text-muted mb-1">Absen: 9</p>
+                <span class="badge bg-secondary">Developer</span>
+            </div>
+        </div>
+    </div>
+    <div class="col">
+        <div class="card border shadow-sm">
+            <div class="card-body text-center">
+                <h5 class="card-title fw-bold">Evan</h5>
+                <p class="card-text text-muted mb-1">Absen: 10 </p>
+                <span class="badge bg-dark">Developer</span>
             </div>
         </div>
     </div>
