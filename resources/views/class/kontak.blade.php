47e0b917d0cec3cdb28f50c4f3649f86df172898
elsadwilasmana2122-cmyk <muarajayaabadi234>
2026-08-27 10:59:22 +0700

    Perubahan Kontak Elsa

--- resources/views/class/kontak.blade.php
+++ resources/views/class/kontak.blade.php
@@ -6,21 +6,68 @@
 <h3 class="fw-bold mb-4">Hubungi Kami</h3>
 
 <div class="row">
-    <div class="col-md-6">
+    <div class="col-md-3">
         <div class="card p-3 border shadow-sm">
             <div class="mb-3">
                 <label class="form-label">Nama</label>
-                <input type="text" class="form-control" placeholder="Masukkan nama">
+                <label> Muhaimin </label>
+            </div>
+            <div class="mb-3">
+                <label class="form-label">Instagram</label>
+                <label>@mhmnardh</label>
             </div>
             <div class="mb-3">
                 <label class="form-label">Email</label>
-                <input type="email" class="form-control" placeholder="nama@email.com">
+                <label>Hasanmuqaffi@gmail.com</label>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-3">
+        <div class="card p-3 border shadow-sm">
+            <div class="mb-3">
+                <label class="form-label">Nama</label>
+                <label> Elsa </label>
             </div>
             <div class="mb-3">
-                <label class="form-label">Pesan</label>
-                <textarea class="form-control" rows="3" placeholder="Tuliskan pesan"></textarea>
+                <label class="form-label">Instagram</label>
+                <label>@dw1elll</label>
             </div>
-            <button type="button" class="btn btn-primary">Kirim Pesan</button>
+            <div class="mb-3">
+                <label class="form-label">Email</label>
+                <label>elsadwilasmana2122@gmail.com</label>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-3">
+        <div class="card p-3 border shadow-sm">
+            <div class="mb-3">
+                <label class="form-label">Nama</label>
+                <label> Bunga</label>
+            </div>
+            <div class="mb-3">
+                <label class="form-label">Instagram</label>
+                <label>@ayuni.bunga</label>
+            </div>
+            <div class="mb-3">
+                <label class="form-label">Email</label>
+                <label>bungaayuni270@gmail.com</label>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-3">
+        <div class="card p-3 border shadow-sm">
+            <div class="mb-3">
+                <label class="form-label">Nama</label>
+                <label> Evan</label>
+            </div>
+            <div class="mb-3">
+                <label class="form-label">Instagram</label>
+                <label>@evanadtyaa_</label>
+            </div>
+            <div class="mb-3">
+                <label class="form-label">Email</label>
+                <label>evanaditya1123@gmail.com</label>
+            </div>
         </div>
     </div>
 </div>
