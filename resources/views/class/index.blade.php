ee6d42437c52f5561f6d26fa63b03cf6a1c0cae0
evan <evanaditya1123@gmail.com>
2026-08-27 10:32:43 +0700

    Perubahan profil

--- resources/views/class/index.blade.php
+++ resources/views/class/index.blade.php
@@ -6,6 +6,11 @@
 <div class="p-5 bg-light rounded-3 border">
     <h1 class="display-6 fw-bold">Selamat Datang di profil kelompok</h1>
     <p class="fs-5 text-muted">Ini adalah website landing page untuk menampilkan profil kelompok, daftar anggota kelompok, dan kontak kelompok.</p>
+    <P>Nama Kelas: XI RPL 1</P>
+    <P>Nama Sekolah: SMKN 1 Garut </P>
+    <P>Program Keahlian: Rekayasa Perangkat Lunak</P>
+    <P>Deskripsi Kelas: Kelas 11 RPL 1 </P>
+    <P>Visi Kelas: Menjadi kelas yang unggul dalam bidang Rekayasa Perangkat Lunak.</P>
     <a href="{{ route('class.kontak') }}" class="btn btn-primary">Lihat Kontak</a>
 </div>
 @endsection
