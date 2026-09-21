e0889144b8609f3e407b17e8cbe1baa429f71661
TristanMZ <tristanmz359@gmail.com>
2026-09-21 14:49:52 +0700

    vercel 7

--- api/index.php
+++ api/index.php
@@ -1,4 +1,18 @@
 <?php
 
-// Forward request ke public/index.php milik Laravel
-require __DIR__ . '/../public/index.php';
+// Load autoload bawaan composer
+require __DIR__ . '/../vendor/autoload.php';
+
+// Jalankan aplikasi Laravel
+$app = require_once __DIR__ . '/../bootstrap/app.php';
+
+// Tangani request yang masuk
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+
+$response = $kernel->handle(
+    $request = Illuminate\Http\Request::capture()
+);
+
+$response->send();
+
+$kernel->terminate($request, $response);
